0798c15dd7a95e71f69e1256e3acba46823c2998
Marsel Zaripov <marszaripov@gmail.com>
2016-07-27 10:13:30 +0300

    remove south related code

--- redactor/fields.py
+++ redactor/fields.py
@@ -25,8 +25,3 @@ class RedactorField(TextField):
         if defaults['widget'] == admin_widgets.AdminTextareaWidget:
             defaults['widget'] = self.widget
         return super(RedactorField, self).formfield(**defaults)
-
-
-if 'south' in settings.INSTALLED_APPS:
-    from south.modelsinspector import add_introspection_rules
-    add_introspection_rules([], ["^redactor\.fields\.RedactorField"])
